e08c1d61a0c2f5f72168710967f5865cf6b1fc9c
Diana Di Stefano <diana.distefano@gmail.com>
2020-08-19 04:17:29 +0200

    Creacion de variables, prompt, consolelog y alert con javascript

---
+++ js/codigo.js
@@ -0,0 +1,16 @@
+var nombre=prompt('¡Hola! ¿Cuál es tu nombre?')
+var numeroA=100
+
+console.log('Hola '+nombre+'. Espero que estés bien');
+
+var edad=parseInt(prompt('Ahora, dime tu edad'))
+
+var suma=edad+numeroA
+
+console.log('Por si no sabías, tu edad ('+edad+') más 100 da como resultado '+suma);
+
+var lugar=prompt('¿En qué lugar estás viviendo actualmente?')
+
+var profesion=prompt('En pocas palabras, ¿con qué profesión te identificarías mejor en el último tiempo?')
+
+console.log('Actualmente, estás viviendo en '+lugar+' y en pocas palabras te definirías como '+profesion);
